3fc63faa580df9e1180d6056d7346227dfe3415a
Majnik <majnik999@gmail.com>
2025-03-09 15:26:36 +0100

    Update...

--- last/code_box.js
+++
@@ -1,9 +0,0 @@
-function copyCode(button) {
-    const codeElement = button.parentElement.nextElementSibling.querySelector("code");
-    const textArea = document.createElement("textarea");
-    textArea.value = codeElement.innerText;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    document.body.removeChild(textArea);
-}
